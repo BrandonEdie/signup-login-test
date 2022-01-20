56bbfe39fb12d258050201b4d1d8fabb8713cd5e
BrandonEdie <82047545+BrandonEdie@users.noreply.github.com>
2022-01-20 11:49:13 -0500

    Update ValidationLogin.js

--- src/ValidationLogin.js
+++ src/ValidationLogin.js
@@ -34,7 +34,7 @@ function signIn(e) {
         alert("Incorrect login credentials");
     }
     else{
-        location.href = "./src/index.html";
+        location.href = "./src/Welcome-Page.html";
     }
     e.preventDefault();
 }
